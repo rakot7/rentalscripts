7a4bb8a193ce523262cc20fa5dbdd4994c72c9ea
rakot7 <o.z313nsky@gmail.com>
2024-10-13 23:39:31 +0200

    Update i-qub-apool-aleo-zk.sh

--- i-qub-apool-aleo-zk.sh
+++ i-qub-apool-aleo-zk.sh
@@ -36,5 +36,6 @@ EOF
 chmod +x ./run.sh
 screen -dmS qub ./run.sh
 echo "" >> /etc/supervisor/conf.d/supervisord.conf
+echo "" >> /etc/supervisor/conf.d/supervisord.conf
 echo "[program:qub]" >> /etc/supervisor/conf.d/supervisord.conf
 echo "command=/bin/bash -c 'cd /root/qub/ap/ && screen -dmS qub ./run.sh && sleep infinity'" >> /etc/supervisor/conf.d/supervisord.conf
