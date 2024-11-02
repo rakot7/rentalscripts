88f434ac0c8a37b61596327f2248f3ab49405c36
rakot7 <o.z313nsky@gmail.com>
2024-11-02 18:25:12 +0200

    Update i-qub-qli-aleo-zk.sh

--- i-qub-qli-aleo-zk.sh
+++ i-qub-qli-aleo-zk.sh
@@ -18,7 +18,7 @@ cat <<EOF > appsettings.json
     "baseUrl": "https://mine.qubic.li/",
     "amountOfThreads": 0,
     "payoutId": null,
-    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTcyOTk1NDI1MiwiZXhwIjoxNzYxNDkwMjUyLCJpYXQiOjE3Mjk5NTQyNTIsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.U7G2c0E5Vdc7I5fEwq7gzGvK2aF4FUkltNU3S6BUuv_aF9Gf3kCqQBGsO5GZihbKeZG_Jssv_mldLhI44TBDD1oxP0jbXqLESfbbC4Qw8ihNNF6WxoKDhA6CrUSXcoT_PWB24bXtuOGbWuWCnM4M-bU2ZbwnAUpwrL1BVxd-1gI88iajCxcBJN_WP6ARAjjSN_Ih42OCZ1yOF0bcu-kU0t1GdtkYQEuk3gMmFXJkZo6xobD1jMjO3Pfq0ubM1KIzGUfE6GuXRxYLLph1z1S6W_6avTizSsoJhPL98ZsNBOEjOACou1qp7DA3m7GvyPrguyqF5p2H-G0E1FtfRnAvhA",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTczMDU2NDY3OSwiZXhwIjoxNzYyMTAwNjc5LCJpYXQiOjE3MzA1NjQ2NzksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AEqraoKlGlzPTaQ4r3L-hWVZOheYJFF-3ShJkLqL0SIMmnKMZZCIR5mUT6TEsF3Rknqj6kFwKrHt4Q_oDlPoz9VZfMn80rJkYazm20ownKtLKx5FPfh0tZY9PlBSLurjZ6tGxaAT-s5mDERWiFAXd7Es15FgOriEQISi1FaboIl5P8yH-rWkihmh8WpdEgk7lqQZQMD8AJ0I0UCaCSNDrazQSf9BKTQFl8keW71OmoOwmank5qytHWx5P9-nDLxKyadbWynZvyucaVZuAmAZs1I4GMy_yWWQA_dVLEDN_IaUTaXzgO2eExoDBuzD9My1EjuVvyyVnr2GA-ICCI8-2w",
     "alias": "$1",
     "trainer": {"gpu": true,"gpuVersion": "CUDA12","cpu": false},
     "idleSettings": {"command": "./aleo_prover","arguments": "--pool aleo.hk.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1"}
