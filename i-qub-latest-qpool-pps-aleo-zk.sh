c44ee62c3b98651c42343d819ffb38aee355ee74
rakot7 <o.z313nsky@gmail.com>
2024-11-07 20:11:29 +0200

    Update i-qub-latest-qpool-pps-aleo-zk.sh

--- i-qub-latest-qpool-pps-aleo-zk.sh
+++ i-qub-latest-qpool-pps-aleo-zk.sh
@@ -5,36 +5,31 @@ mkdir qub
 cd qub
 mkdir qli
 wget https://github.com/qubic-li/hiveos/releases/download/beta/qubminer.beta-latest.tar.gz
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.4/aleo_prover-v0.2.4_cuda_full.tar.gz
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.2.3-fix/aleo_prover-v0.2.3_full_fix.tar.gz
 tar -C qli -xf qubminer.beta-latest.tar.gz
-cp ./qli/qubminer.beta/* ./qli/
-tar -xf aleo_prover-v0.2.4_cuda_full.tar.gz aleo_prover/aleo_prover
+cd qli 
+cp ./qubminer.beta/* ./
+rm -R ./qubminer.beta/
+tar -xf aleo_prover-v0.2.3_full_fix.tar.gz aleo_prover/aleo_prover
 cp ./aleo_prover/aleo_prover ./qli/aleo_prover
 rm -R aleo_prover
-rm -R ./qli/qubminer.beta
 cd qli
 rm appsettings.json
 cat <<EOF > appsettings.json
 {
-  "Settings": {
-    "baseUrl": "https://wps.qubic.li",
-    "alias": "$1",
-    "trainer": {
-      "cpu": true,
-      "gpu": true,
-      "gpuVersion": "CUDA12",
-      "cpuVersion": "avx512",
-      "cpuThreads": 0
-    },
-    "isPps": false,
-    "useLiveConnection": true,
-    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNzU2MTYxMSwiZXhwIjoxNzU5MDk3NjExLCJpYXQiOjE3Mjc1NjE2MTEsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.GtVw1cTrW7_D7EDOyebbtuHo0Xdpy93ZP0S_6aSswVN0BmSiwHCNA9-s2qSottV_RWpAysz_yrHks5iWcuN3yOsbvpMZwWA44HelEdV7dtSUmbaO6tsZtVVmrNL_MCDcFaYVlVjp4nLccJtmUnYfidYsjZog0apFj4t-Ub7HdB313iCoohNRBiFmoGKQg6pSp4xQtTKfI9i6oGJXEVhZyYV_KHCCA2mBL6e5-KhtyuSh6-t0GRrhy_wro5FcdkAZq1si6O_-R86SedVE3O1u64gQPRJbIzPzgimY9yZ56bZYh5StCDJMENU79KkRT3ErL_PsIJ612Bou5HIQg-KHbA",
-    "idleSettings": {
-      "command": "/root/qub/qli/aleo_prover",
-      "arguments": "--pool aleo.hk.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1"
-    }
+  "ClientSettings": {
+      "poolAddress": "wss://wps.qubic.li/ws",
+      "alias": "$1",
+      "trainer":{"cpu":false,"gpu":true},
+      "pps":false,
+      "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTczMDk4OTI5NSwiZXhwIjoxNzYyNTI1Mjk1LCJpYXQiOjE3MzA5ODkyOTUsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.NE1NAUKP11sUJtpigTBEnWNlFfovMYWTwysPbV1fL0wVexUmyDlPUMYugoQJj8d0g5luYgviCPnx3IS01DYzrqKh7j8FEQ5kYt1AblgXJtKFA8Z7hfw8_sc4JIA62fjdv7MVPJmdPoY3i2ang7gvIOBWvG32mj8Avtm32MKMrZfB0yKFX1QDpHBXvnsLtSszPbWz5kFFgO_mAk5qs7p4u7AgNqkSbF4-_S2JzFpo_5XIGOm-_609ifnz14EOlU41j3utUyl09xD394inJAGDPY8oLsuYhwfyDDA8tbSfyEwb2ck1qHlE5gM86uSWlceyTpLHwDB9BsQdCTPtylNaaQ",
+      "qubicAddress": null,
+      "displayDetailedHashrates": true,
+      "idling":{"command":"./aleo_prover","arguments":"--pool aleo.asia1.zk.work:10003 --pool aleo.hk.zk.work:10003 --pool aleo.jp.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1"}
+
   }
 }
+AutoUpdate
 EOF
 chmod +x ./qli-Client
 screen -dmS qub ./qli-Client
