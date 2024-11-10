f1caebe279da34c29a07e4911246bba6baab5a1b
rakot7 <o.z313nsky@gmail.com>
2024-11-10 10:07:34 +0200

    Update i-qub-qli-aleo-zk.sh

--- i-qub-qli-aleo-zk.sh
+++ i-qub-qli-aleo-zk.sh
@@ -5,9 +5,9 @@ mkdir qub
 cd qub
 mkdir qli
 wget https://dl.qubic.li/downloads/qli-Client-2.2.1-Linux-x64.tar.gz
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.2.3-fix/aleo_prover-v0.2.3_full_fix.tar.gz
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.4/aleo_prover-v0.2.4_cuda_full.tar.gz
 tar -C qli -xf qli-Client-2.2.1-Linux-x64.tar.gz
-tar -xf aleo_prover-v0.2.3_full_fix.tar.gz aleo_prover/aleo_prover
+tar -xf aleo_prover-v0.2.4_cuda_full.tar.gz aleo_prover/aleo_prover
 cp ./aleo_prover/aleo_prover ./qli/aleo_prover
 rm -R aleo_prover
 cd qli
@@ -15,10 +15,10 @@ rm appsettings.json
 cat <<EOF > appsettings.json
 {
   "Settings": {
-    "baseUrl": "https://mine.qubic.li/",
+    "baseUrl": "https://wps.qubic.li/",
     "amountOfThreads": 0,
     "payoutId": null,
-    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTczMDU2NDY3OSwiZXhwIjoxNzYyMTAwNjc5LCJpYXQiOjE3MzA1NjQ2NzksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AEqraoKlGlzPTaQ4r3L-hWVZOheYJFF-3ShJkLqL0SIMmnKMZZCIR5mUT6TEsF3Rknqj6kFwKrHt4Q_oDlPoz9VZfMn80rJkYazm20ownKtLKx5FPfh0tZY9PlBSLurjZ6tGxaAT-s5mDERWiFAXd7Es15FgOriEQISi1FaboIl5P8yH-rWkihmh8WpdEgk7lqQZQMD8AJ0I0UCaCSNDrazQSf9BKTQFl8keW71OmoOwmank5qytHWx5P9-nDLxKyadbWynZvyucaVZuAmAZs1I4GMy_yWWQA_dVLEDN_IaUTaXzgO2eExoDBuzD9My1EjuVvyyVnr2GA-ICCI8-2w",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTczMTIyNjAzNiwiZXhwIjoxNzYyNzYyMDM2LCJpYXQiOjE3MzEyMjYwMzYsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.RUhyNysvMNGd3agNfBtN5iVVmvbA2Cx9nEKv6WK5rNJFeW9WYAkxTbvN2RXN0F8bMmX7k6HC515UXyge9XbrrGvt-d0oPhGlZh7HHBKPCSAHmoK-LtTkJNCLYSV6vIhOUvH3xEthWg6Y313Ljx24qDGzmQF_DwF-77ETkbYYoWvDRbyjS8Zg3dEReEM7_S6lx7_SJqnYhJljDZ7CyVzHoPc_YRCMffdUbqlJH0JOwcanK5h8ibcIo0_pJ2Ep2frG36aVNmgFuiCQW-Oa_RyiZcEdTxsxbQc-tgucADxmpQK-_eviXRVukVCwstjfSroPsNfhZKM2fvPTZszqCQcrgA",
     "alias": "$1",
     "trainer": {"gpu": true,"gpuVersion": "CUDA12","cpu": false},
     "idleSettings": {"command": "./aleo_prover","arguments": "--pool aleo.asia1.zk.work:10003 --pool aleo.hk.zk.work:10003 --pool aleo.jp.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1"}
