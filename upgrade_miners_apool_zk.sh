afa5e9dfb67d8f578fb2314b5a2ef1efb9730704
rakot7 <o.z313nsky@gmail.com>
2024-11-18 10:43:31 +0200

    Update upgrade_miners_apool_zk.sh

--- upgrade_miners_apool_zk.sh
+++ upgrade_miners_apool_zk.sh
@@ -6,13 +6,13 @@ tar -xf apoolminer_linux_autoupdate_v2.6.6.tar.gz
 tar -xf aleo_prover-v0.2.5_cuda_full.tar.gz aleo_prover/aleo_prover
 rm ./ap/aleo_prover
 rm ./ap/apoolminer
+pkill -f 'run.sh'
+pkill -f 'apoolminer'
+pkill -f 'aleo_prover'
 cp ./aleo_prover/aleo_prover ./ap/
 cp ./apoolminer_linux_autoupdate_v2.6.6/apoolminer ./ap/
 rm -R apoolminer_linux_autoupdate_v2.6.6
 rm -R aleo_prover
 cd ap
-pkill -f 'run.sh'
-pkill -f 'apoolminer'
-pkill -f 'aleo_prover'
 screen -dmS qub ./run.sh
 screen -r
