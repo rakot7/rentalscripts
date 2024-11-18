af0b8933a76f296e1da9ceaecb02d19037e2dbe3
rakot7 <o.z313nsky@gmail.com>
2024-11-18 10:30:39 +0200

    Create upgrade_miners_apool_zk.sh

---
+++ upgrade_miners_apool_zk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /root/qub/
+wget https://github.com/apool-io/apoolminer/releases/download/v2.6.6/apoolminer_linux_autoupdate_v2.6.6.tar.gz
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5/aleo_prover-v0.2.5_cuda_full.tar.gz
+tar -xf apoolminer_linux_autoupdate_v2.6.6.tar.gz
+tar -xf aleo_prover-v0.2.5_cuda_full.tar.gz aleo_prover/aleo_prover
+rm ./ap/aleo_prover
+rm ./ap/apoolminer
+cp ./aleo_prover/aleo_prover ./ap/
+cp ./apoolminer_linux_autoupdate_v2.6.6/apoolminer ./ap/
+rm -R apoolminer_linux_autoupdate_v2.6.6
+rm -R aleo_prover
+cd ap
+pkill -f 'run.sh'
+pkill -f 'apoolminer'
+pkill -f 'aleo_prover'
+screen -dmS qub ./run.sh
+screen -r
