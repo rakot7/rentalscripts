a3df76004c164ccb3311a9fd185e6e535b3d2fce
rakot7 <o.z313nsky@gmail.com>
2024-11-18 13:22:49 +0200

    Update i-qub-apool-ore-gpool.sh

--- i-qub-apool-ore-gpool.sh
+++ i-qub-apool-ore-gpool.sh
@@ -3,15 +3,15 @@ apt update -y && apt install -y screen git curl cron nano mc htop iputils-ping
 cd /root/
 mkdir qub
 cd qub
-wget https://github.com/apool-io/apoolminer/releases/download/v2.6.5/apoolminer_linux_autoupdate_v2.6.5.tar.gz
+wget https://github.com/apool-io/apoolminer/releases/download/v2.6.6/apoolminer_linux_autoupdate_v2.6.6.tar.gz
 wget https://github.com/gpool-cloud/gpool-cli/raw/refs/heads/main/gpool
 rm -R /root/qub/ap
 mkdir ap
-tar -xf apoolminer_linux_autoupdate_v2.6.5.tar.gz
+tar -xf apoolminer_linux_autoupdate_v2.6.6.tar.gz
 cp gpool ./ap/
 chmod +x ./ap/gpool
-cp ./apoolminer_linux_autoupdate_v2.6.5/* ./ap/
-rm -R apoolminer_linux_autoupdate_v2.6.5
+cp ./apoolminer_linux_autoupdate_v2.6.6/* ./ap/
+rm -R apoolminer_linux_autoupdate_v2.6.6
 cd ap
 rm miner.conf
 rm run.sh
