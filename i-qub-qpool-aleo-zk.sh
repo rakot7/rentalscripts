f4f82e5fc984555b5c0b4db50ee7f794a2b1f844
rakot7 <o.z313nsky@gmail.com>
2024-11-20 22:56:21 +0200

    Update i-qub-qpool-aleo-zk.sh

--- i-qub-qpool-aleo-zk.sh
+++ i-qub-qpool-aleo-zk.sh
@@ -5,9 +5,9 @@ mkdir qub
 cd qub
 mkdir qli
 wget https://dl.qubic.li/downloads/qli-Client-2.2.1-Linux-x64.tar.gz
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.2.3-fix/aleo_prover-v0.2.3_full_fix.tar.gz
+wget --continue --tries=0 https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
 tar -C qli -xf qli-Client-2.2.1-Linux-x64.tar.gz
-tar -xf aleo_prover-v0.2.3_full_fix.tar.gz aleo_prover/aleo_prover
+tar -xf aleo_prover-v0.2.5_cuda_full_hotfix aleo_prover/aleo_prover
 cp ./aleo_prover/aleo_prover ./qli/aleo_prover
 rm -R aleo_prover
 cd qli
@@ -24,12 +24,12 @@ cat <<EOF > appsettings.json
       "cpuVersion": "",
       "cpuThreads": 0
     },
-    "isPps": false,
+    "isPps": true,
     "useLiveConnection": true,
-    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNzU2MTYxMSwiZXhwIjoxNzU5MDk3NjExLCJpYXQiOjE3Mjc1NjE2MTEsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.GtVw1cTrW7_D7EDOyebbtuHo0Xdpy93ZP0S_6aSswVN0BmSiwHCNA9-s2qSottV_RWpAysz_yrHks5iWcuN3yOsbvpMZwWA44HelEdV7dtSUmbaO6tsZtVVmrNL_MCDcFaYVlVjp4nLccJtmUnYfidYsjZog0apFj4t-Ub7HdB313iCoohNRBiFmoGKQg6pSp4xQtTKfI9i6oGJXEVhZyYV_KHCCA2mBL6e5-KhtyuSh6-t0GRrhy_wro5FcdkAZq1si6O_-R86SedVE3O1u64gQPRJbIzPzgimY9yZ56bZYh5StCDJMENU79KkRT3ErL_PsIJ612Bou5HIQg-KHbA",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTczMjEzNjAzNSwiZXhwIjoxNzYzNjcyMDM1LCJpYXQiOjE3MzIxMzYwMzUsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.XSiV-hMVN9LPLS1KoiaqA2tNy-446MP8jOCCRhY33M3Qlj0TAvhOvT3wDgEgYpqXJEhco5ZquE0BB2_batfGWJm5LqjYD1HVDSPUk83tfTmDbrQFNmXLGw134r3dIrOtRd3HoD2D5KaYzivPSalrYwVyHU6eS6ww-lZKwoU5jmetgE8OU6n3aEmPAqpt2V0EPmtNKZr88AAxN-qJ2IDxGF08AuH7IzahERByAMflZKNxjI-dTu4vxYwGvHSSFztKb1QzKmMWAAvjrS4Kh0JvqYl6DPxZY_QFKBh8OXoc25uW240dmLWz-WUZXHiTM6Hj_3ZZzXfvCrX14d5Jh0DnFA",
     "idleSettings": {
       "command": "/root/qub/qli/aleo_prover",
-      "arguments": "--pool aleo.hk.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1"
+      "arguments": "--pool aleo.asia1.zk.work:10003 --pool aleo.hk.zk.work:10003 --pool aleo.jp.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1"
     }
   }
 }
