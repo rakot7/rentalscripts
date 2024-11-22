6bf7fde80e402ee74b6bad5ea4dcb4cf4987490f
rakot7 <o.z313nsky@gmail.com>
2024-11-22 21:27:59 +0200

    Update i-qub-latest-qpool-no-pps-aleo-zk.sh

--- i-qub-latest-qpool-no-pps-aleo-zk.sh
+++ i-qub-latest-qpool-no-pps-aleo-zk.sh
@@ -4,9 +4,9 @@ cd ~
 mkdir qub
 cd qub
 wget https://github.com/qubic-li/hiveos/releases/download/beta/qubminer.beta-latest.tar.gz
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.4/aleo_prover-v0.2.4_cuda_full.tar.gz
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix/aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz
 tar -xf qubminer.beta-latest.tar.gz
-tar -xf aleo_prover-v0.2.4_cuda_full.tar.gz aleo_prover/aleo_prover
+tar -xf aleo_prover-v0.2.5_cuda_full_hotfix.tar.gz aleo_prover/aleo_prover
 cp ./aleo_prover/aleo_prover ./qubminer.beta/aleo_prover
 rm -R aleo_prover
 cd qubminer.beta 
@@ -17,7 +17,7 @@ cat <<EOF > appsettings.json
       "poolAddress": "wss://wps.qubic.li/ws",
       "alias": "$1",
       "trainer":{"cpu":false,"gpu":true},
-      "pps":false,
+      "pps":true,
       "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTczMDk4OTI5NSwiZXhwIjoxNzYyNTI1Mjk1LCJpYXQiOjE3MzA5ODkyOTUsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.NE1NAUKP11sUJtpigTBEnWNlFfovMYWTwysPbV1fL0wVexUmyDlPUMYugoQJj8d0g5luYgviCPnx3IS01DYzrqKh7j8FEQ5kYt1AblgXJtKFA8Z7hfw8_sc4JIA62fjdv7MVPJmdPoY3i2ang7gvIOBWvG32mj8Avtm32MKMrZfB0yKFX1QDpHBXvnsLtSszPbWz5kFFgO_mAk5qs7p4u7AgNqkSbF4-_S2JzFpo_5XIGOm-_609ifnz14EOlU41j3utUyl09xD394inJAGDPY8oLsuYhwfyDDA8tbSfyEwb2ck1qHlE5gM86uSWlceyTpLHwDB9BsQdCTPtylNaaQ",
       "qubicAddress": null,
       "displayDetailedHashrates": true,
