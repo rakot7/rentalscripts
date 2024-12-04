bf26e7792ceb04e59f4d1d59b8a24cdb7b60997a
rakot7 <o.z313nsky@gmail.com>
2024-12-04 14:41:52 +0200

    Update upgrade_miners_apool_zk.sh

--- upgrade_miners_apool_zk.sh
+++ upgrade_miners_apool_zk.sh
@@ -2,9 +2,9 @@
 cd /root/qub/
 rm apoolminer_linux_autoupdate_v2.6*
 rm leo_prover-v0.2.5.*
-wget --continue --tries=0 https://github.com/apool-io/apoolminer/releases/download/v2.6.7/apoolminer_linux_autoupdate_v2.6.7.tar.gz
+wget --continue --tries=0 https://github.com/apool-io/apoolminer/releases/download/v2.7.0/apoolminer_linux_autoupdate_v2.7.0.tar.gz
 wget --continue --tries=0 https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
-tar -xf apoolminer_linux_autoupdate_v2.6.7.tar.gz
+tar -xf apoolminer_linux_autoupdate_v2.7.0.tar.gz
 tar -xf aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz aleo_prover/aleo_prover
 rm ./ap/aleo_prover
 rm ./ap/apoolminer
@@ -13,8 +13,8 @@ pkill -f 'run.sh'
 pkill -f 'apoolminer'
 pkill -f 'aleo_prover'
 cp ./aleo_prover/aleo_prover ./ap/
-cp ./apoolminer_linux_autoupdate_v2.6.7/apoolminer ./ap/
-rm -R apoolminer_linux_autoupdate_v2.6.7
+cp ./apoolminer_linux_autoupdate_v2.7.0/apoolminer ./ap/
+rm -R apoolminer_linux_autoupdate_v2.7.0
 rm -R aleo_prover
 cd ap
 screen -dmS qub ./run.sh
