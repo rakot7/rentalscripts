01b7d97e5d782c00e9158ff9f493d0e99b541873
rakot7 <o.z313nsky@gmail.com>
2024-12-18 21:49:55 +0200

    Update i-qub-qlab-aleo-zk.sh

--- i-qub-qlab-aleo-zk.sh
+++ i-qub-qlab-aleo-zk.sh
@@ -20,7 +20,7 @@ cat <<EOF > appsettings.json
 {
   "ClientSettings": {
     "poolAddress": "wss://pps.minerlab.io/ws/kotklgd",
-    "alias": "13013",
+    "alias": "$1",
     "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImQzNzMyODc2LTY5ZDctNGI1OC1hNmUzLWM2MzZkMGQ4ZDE0NiIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNTM3NjkyOSwiZXhwIjoxNzU2OTEyOTI5LCJpYXQiOjE3MjUzNzY5MjksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.sregOyk2PEyXv8ssdQDBtTps1JFBLghcJCzDFvaD6hWoVA_T-crfQZbiV0E_atqd6sxNHYKGmeVCOoU9crLU4mnojZdF1vyp3VttB3ZIqo3qIgr0R4jWnwZ95bGN1c6NE3zb9y7ZWor5-4ttLkR_5moxiZZvaKG2WWSxFJ-7kk6SVSw7z8iaYyVpPX1Tdu6pBWxDStYYaoVvgNzx6RShU_r2AVCB1JGfv16vKvAIGmPcluvS-ayKwfgOpY1uEbsH6Lswd_KGbB1aJC7g8AI1CUoYiUUl_CJUBZfG0FbBgtGDRhfPUcYM5z8BEyIrm6bfKhMHuJmIF86NJYydRUHgow",
     "qubicAddress": null,
     "displayDetailedHashrates": true,
@@ -32,7 +32,7 @@ cat <<EOF > appsettings.json
     "Idling": {
       "gpuOnly": true,
       "command": "./aleo_prover",
-      "arguments": "--pool aleo.asia1.zk.work:10003 --pool aleo.hk.zk.work:10003 --pool aleo.jp.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name 13013",
+      "arguments": "--pool aleo.asia1.zk.work:10003 --pool aleo.hk.zk.work:10003 --pool aleo.jp.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1",
       "preCommand": null,
       "preCommandArguments": null,
       "postCommand": null,
