bec4a11d2cd56f1e09bc5d9a9cee7b63b4572969
rakot7 <o.z313nsky@gmail.com>
2024-12-20 13:31:21 +0200

    Update i-qub-qlab-aleo-zk.sh

--- i-qub-qlab-aleo-zk.sh
+++ i-qub-qlab-aleo-zk.sh
@@ -11,7 +11,7 @@ mkdir qlab
 wget --continue --tries=0 https://poolsolution.s3.eu-west-2.amazonaws.com/QLAB.Xminer-latest.tar.gz
 wget --continue --tries=0 https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
 tar -C qlab -xf QLAB.Xminer-latest.tar.gz
-cp QLAB.Xminer/* /root/qub/qlab/
+cp /root/qub/qlab/QLAB.Xminer/* /root/qub/qlab/*
 rm -R QLAB.Xminer/
 tar -xf aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz aleo_prover/aleo_prover
 cp ./aleo_prover/aleo_prover ./qlab/aleo_prover
