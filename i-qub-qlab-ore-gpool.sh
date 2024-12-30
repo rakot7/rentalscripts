528447b8180a8b425d426e3de2fd5096a97df776
rakot7 <o.z313nsky@gmail.com>
2024-12-30 21:31:46 +0200

    Create i-qub-qlab-ore-gpool.sh

---
+++ i-qub-qlab-ore-gpool.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+apt update -y && apt install -y screen git curl cron nano mc htop iputils-ping
+echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list
+apt update
+apt install -y libc6
+apt install -y g++-11
+cd ~
+mkdir qub
+cd qub
+mkdir qlab
+wget --continue --tries=0 https://poolsolution.s3.eu-west-2.amazonaws.com/QLAB.Xminer-latest.tar.gz
+wget --continue --tries=0 https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/cuda-v0.2.5-hotfix2/aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz
+tar -C qlab -xf QLAB.Xminer-latest.tar.gz
+cp /root/qub/qlab/QLAB.Xminer/* /root/qub/qlab/
+rm -R QLAB.Xminer
+tar -xf aleo_prover-v0.2.5_cuda_full_hotfix2.tar.gz aleo_prover/aleo_prover
+cp ./aleo_prover/aleo_prover ./qlab/aleo_prover
+rm -R aleo_prover
+cd qlab
+rm appsettings.json
+cat <<EOF > appsettings.json
+{
+  "ClientSettings": {
+    "poolAddress": "wss://pps.minerlab.io/ws/kotklgd",
+    "alias": "$1",
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImQzNzMyODc2LTY5ZDctNGI1OC1hNmUzLWM2MzZkMGQ4ZDE0NiIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNTM3NjkyOSwiZXhwIjoxNzU2OTEyOTI5LCJpYXQiOjE3MjUzNzY5MjksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.sregOyk2PEyXv8ssdQDBtTps1JFBLghcJCzDFvaD6hWoVA_T-crfQZbiV0E_atqd6sxNHYKGmeVCOoU9crLU4mnojZdF1vyp3VttB3ZIqo3qIgr0R4jWnwZ95bGN1c6NE3zb9y7ZWor5-4ttLkR_5moxiZZvaKG2WWSxFJ-7kk6SVSw7z8iaYyVpPX1Tdu6pBWxDStYYaoVvgNzx6RShU_r2AVCB1JGfv16vKvAIGmPcluvS-ayKwfgOpY1uEbsH6Lswd_KGbB1aJC7g8AI1CUoYiUUl_CJUBZfG0FbBgtGDRhfPUcYM5z8BEyIrm6bfKhMHuJmIF86NJYydRUHgow",
+    "qubicAddress": null,
+    "displayDetailedHashrates": true,
+    "trainer": {
+      "cpu": false,
+      "gpu": true
+    },
+    "pps":true,
+    "Idling": {
+      "gpuOnly": true,
+      "command": "./aleo_prover",
+      "arguments": "--pool aleo.asia1.zk.work:10003 --pool aleo.hk.zk.work:10003 --pool aleo.jp.zk.work:10003 --address aleo1p5063azmcd5ajzr3nmp9u6ezpta5e9wq7a0dnq5h75vm26x0h58st00ws2 --custom_name $1",
+      "preCommand": null,
+      "preCommandArguments": null,
+      "postCommand": null,
+      "postCommandArguments": null
+    }
+  }
+}
+
+EOF
+chmod +x ./qli-Client
+screen -dmS qub ./qli-Client
+echo "" >> /etc/supervisor/conf.d/supervisord.conf
+echo "[program:qub]" >> /etc/supervisor/conf.d/supervisord.conf
+echo "command=/bin/bash -c 'cd /root/qub/qlab/QLAB.Xminer/ && screen -dmS qub ./qli-Client && sleep infinity'" >> /etc/supervisor/conf.d/supervisord.conf
