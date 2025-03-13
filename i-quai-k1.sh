40f177303aaf9dd5114259a9f9045ac8a599c02c
rakot7 <o.z313nsky@gmail.com>
2025-03-13 08:58:09 +0200

    Create i-quai-k1.sh

---
+++ i-quai-k1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ~
+mkdir quai
+cd quai
+wget --continue --tries=0 https://github.com/rigelminer/rigel/releases/download/1.21.2/rigel-1.21.2-linux.tar.gz
+tar -xf rigel-1.21.2-linux.tar.gz
+cd rigel-1.21.2-linux
+rm quai.sh
+cat <<EOF > quai.sh
+#!/bin/bash
+./rigel -a quai -o stratum+tcp://eu.quai.k1pool.com:3333 -u KrTNcwsTe9exc7z9PBbThE533vEJzpfjDGb -w $(hostname)
+EOF
+chmod +x ./quai.sh
+screen -dmS quai ./quai.sh
+echo "" >> /etc/supervisor/conf.d/supervisord.conf
+echo "" >> /etc/supervisor/conf.d/supervisord.conf
+echo "[program:quai]" >> /etc/supervisor/conf.d/supervisord.conf
+echo "command=/bin/bash -c 'cd /root/quai/rigel-1.21.2-linux && screen -dmS quai ./quai.sh && sleep infinity'" >> /etc/supervisor/conf.d/supervisord.conf
