dfb50e5217df05635b171e731f6e964270ca03af
rakot7 <o.z313nsky@gmail.com>
2025-06-27 17:49:38 +0200

    Create i-qub-qpool-tari-lucky.sh

---
+++ i-qub-qpool-tari-lucky.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+apt update -y && apt install -y screen git curl cron nano mc htop iputils-ping
+cd ~
+mkdir qub
+cd qub
+mkdir qli
+wget --continue --tries=0 https://dl.qubic.li/downloads/qli-Client-3.3.8-Linux-x64.tar.gz
+wget --continue --tries=0 https://github.com/doktor83/SRBMiner-Multi/releases/download/2.9.3/SRBMiner-Multi-2-9-3-Linux.tar.gz
+tar -C qli -xf qli-Client-3.3.8-Linux-x64.tar.gz
+tar -C qli -xf SRBMiner-Multi-2-9-3-Linux.tar.gz
+mv ./qli/SRBMiner-Multi-2-9-3/* ./qli/
+rm -r ./qli//SRBMiner-Multi-2-9-3
+cd qli 
+rm appsettings.json
+cat <<EOF > appsettings.json
+{
+  "Settings": {
+    "baseUrl": "https://mine.qubic.li/",
+    "amountOfThreads": 0,
+    "payoutId": null,
+    "accessToken": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY5MTJkOTEwLWRiNDAtNDZmMS04MmI2LTY4OTc5MDQ3ODJmZCIsIk1pbmluZyI6IiIsIm5iZiI6MTc1MTAzOTIwMywiZXhwIjoxNzgyNTc1MjAzLCJpYXQiOjE3NTEwMzkyMDMsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.aXwbnoJqiGe1R9Mf5MzejSxwSGNLI3EEnHsl3mBJqGzk5Civ92eMGxy--P4jy9orVgiaoCZmKdIj_B1akNrLV83hR_X4UR57A8eQwMq4kLV8RQesJpMd49Py6l66nI9OMWkf8JvDdEr337AaTMP15D_ME-gWWqIzm_mJxEUf0CbMpZiT_Xf-cM3kUbrN4Ffy-X948KAzLsIQY4w9BZBeqS0DprPcVmixOUXd8TyIFT3tbXSXi-mP_iDTW_7lm1w5h0YMmwgZUrLUZU47Z9i9A4zzCSzxxqEZG3ulgI0nf6jSTpmqzV_qEuRTHH7MmKvTUaLgbEau1eHjyIABUWVm3Q",
+    "pps": true,
+    "alias": "$(hostname)",
+    "trainer": {"gpu": true,"gpuVersion": "CUDA12","cpu": false},
+    "idleSettings": {"command": "./SRBMiner-MULTI ","arguments": "SRBMiner-MULTI.exe --algorithm sha3x --pool tari.luckypool.io:6118,ca2.luckypool.io:6118,sg3.luckypool.io:6118 --wallet 12FWnbVg1WpMUomitjvtD8rU3sn4oN95VEVZNbFvs8GHevq9H4q2xMPMceQsfQhGUExRnDwcbqxrTBHN9uHisv7nvCj.$(hostname) 0"}
+  }
+}
+AutoUpdate
+EOF
+chmod +x ./qli-Client
+screen -dmS qub ./qli-Client
+echo "" >> /etc/supervisor/conf.d/supervisord.conf
+echo "[program:qub]" >> /etc/supervisor/conf.d/supervisord.conf
+echo "command=/bin/bash -c 'cd /root/qub/qli/ && screen -dmS qub ./qli-Client && sleep infinity'" >> /etc/supervisor/conf.d/supervisord.conf
