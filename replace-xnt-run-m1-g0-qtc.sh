e07fd91a020da9cb10a3ee547d2a2438b5a31b1c
rakot7 <o.z313nsky@gmail.com>
2026-01-16 18:57:32 +0200

    Update replace-xnt-run-m1-g0-qtc.sh

--- replace-xnt-run-m1-g0-qtc.sh
+++ replace-xnt-run-m1-g0-qtc.sh
@@ -15,7 +15,7 @@ APP_PATH=.\$APPNMAE
 pkill -9 \$APPNMAE
 
 
-./xntprover -p stratum+tcp://xnt.drpool.io:30120 -w kotklgd.$(hostname) -m 1 -g 0 --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >>/${CUSTOM_LOG_BASENAME}.log 2>&1
-echo "./xntprover -p stratum+tcp://xnt.drpool.io:30120 -w kotklgd.$(hostname) -m 1 -g 0 --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >> /${CUSTOM_LOG_BASENAME}.log 2>&1"
+./xntprover -p stratum+tcp://xnt.drpool.io:30120 -w kotklgd.$(hostname) -m 1 -g 0 --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >>\${CUSTOM_LOG_BASENAME}.log 2>&1
+echo "./xntprover -p stratum+tcp://xnt.drpool.io:30120 -w kotklgd.$(hostname) -m 1 -g 0 --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >> \${CUSTOM_LOG_BASENAME}.log 2>&1"
 EOF
 chmod +x ./h-run.sh 
