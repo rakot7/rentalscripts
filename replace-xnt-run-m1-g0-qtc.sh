5cce98aafa2a77c78acb34f64bf974c4a39d1d37
rakot7 <o.z313nsky@gmail.com>
2026-01-16 18:51:14 +0200

    Update replace-xnt-run-m1-g0-qtc.sh

--- replace-xnt-run-m1-g0-qtc.sh
+++ replace-xnt-run-m1-g0-qtc.sh
@@ -8,11 +8,11 @@ cat <<EOF > h-run.sh
 #!/bin/bash
 
 source h-manifest.conf
-source $CUSTOM_CONFIG_FILENAME
-APPNMAE=$CUSTOM_NAME
-APP_PATH=.$APPNMAE
+source \$CUSTOM_CONFIG_FILENAME
+APPNMAE=\$CUSTOM_NAME
+APP_PATH=.\$APPNMAE
 
-pkill -9 $APPNMAE
+pkill -9 \$APPNMAE
 
 
 ./xntprover -p stratum+tcp://xnt.drpool.io:30120 -w kotklgd.$(hostname)  -m 1 -g 0 --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >>${CUSTOM_LOG_BASENAME}.log 2>&1
