6de30121829ee75828eb4a1b3f0f194d53196ec7
rakot7 <o.z313nsky@gmail.com>
2026-02-05 10:58:36 +0200

    Update replace-npt-run-m2-g0-qtc.sh

--- replace-npt-run-m2-g0-qtc.sh
+++ replace-npt-run-m2-g0-qtc.sh
@@ -8,11 +8,11 @@ mv h-run.sh h-run.sh.orig
 cat <<EOF > h-run.sh
 #!/bin/bash
 source h-manifest.conf
-source $CUSTOM_CONFIG_FILENAME
+source \$CUSTOM_CONFIG_FILENAME
 APPNMAE=$CUSTOM_NAME
 APP_PATH=./$APPNMAE
 
-pkill -9 $APPNMAE
+pkill -9 \$APPNMAE
 
 $APP_PATH -p $CUSTOM_URL -w $ACCOUNT $CUSTOM_USER_CONFIG --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >>${CUSTOM_LOG_BASENAME}.log 2>&1
 echo "$APP_PATH -p $CUSTOM_URL -w $ACCOUNT $CUSTOM_USER_CONFIG --extra 'onezerominer;-a;qhash;-w;bc1qg4vaek7aqu9jkf0c7epf6lm6wsf8z5c0x9rz22.$(hostname);-o;stratum+tcp://qubitcoin.luckypool.io:8610' >> ${CUSTOM_LOG_BASENAME}.log 2>&1"
