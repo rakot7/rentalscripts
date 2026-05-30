31900cfea59f3798f95fa053c0a4c51830be2a9c
rakot7 <o.z313nsky@gmail.com>
2026-05-30 09:05:14 +0200

    Create i-pearl-alpha.sh

---
+++ i-pearl-alpha.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+apt update -y
+apt install -y screen btop moreutils curl
+echo 'root:Q!@We34rt56y' | chpasswd
+cd /
+cd root
+mkdir pearl
+cd pearl
+wget -c -t=0 https://github.com/rakot7/distros/raw/refs/heads/main/alpha-miner-beta-175.tar.gz
+tar -xf alpha-miner-beta-175.tar.gz
+mv alpha-miner-beta-175 alpha-miner;
+chmod +x alpha-miner
+rm pearl.sh;
+cat <<EOF > pearl.sh
+#!/bin/bash
+while true; do
+        ./alpha-miner --pool stratum+tcp://ru1.alphapool.tech:5566 --address prl1pqyjpfkk3cexjdzt998necuce8m60ep7kc3aru4jard5067k9q88s0qrgse --worker $(hostname)  &
+        PID=\$!
+        sleep 12h
+        kill \$PID
+        sleep 15s;
+done;
+EOF
+chmod +x ./pearl.sh
+screen -dmS p ./pearl.sh 
+rm /etc/supervisor/conf.d/supervisord.conf
+cat <<EOF > /etc/supervisor/conf.d/supervisord.conf
+[supervisord]
+nodaemon=true
+[program:sshd]
+command=/usr/sbin/sshd -D
+[program:jupyter]
+command=/bin/bash -c 'jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser'
+[program:delegated_entrypoint]
+command=/bin/bash /etc/delegated-entrypoint.sh
+[program:pearl]
+command=/bin/bash -c 'cd /root/pearl && screen -dmS pearl ./pearl.sh && sleep infinity'
+EOF
+screen -r p
