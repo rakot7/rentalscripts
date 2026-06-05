0242d6932e0e6829658e3c7078d1a7d4fe7fb261
rakot7 <o.z313nsky@gmail.com>
2026-06-05 20:57:33 +0200

    Update deoz.sh

--- deoz.sh
+++ deoz.sh
@@ -1,10 +1,7 @@
+
 #!/bin/bash
 
 srv=$1
-
-
-
-
 
 while true; do
 
@@ -24,24 +21,6 @@ curl -XPOST -H 'auth: B8u_l8xhBMeB3RIaMXp1MWMm9YBUfgNO' -H "Content-type: applic
     "command":"#!/bin/sh\napt update -y && apt install htop"
 }' 'https://api.clore.ai/v1/create_order'
 
-
-
-
-
-
-#curl --location --request POST 'https://api.clore.ai/v1/edit_gigaspot_orders' \
-#--header 'auth: B8u_l8xhBMeB3RIaMXp1MWMm9YBUfgNO' \
-#--header 'User-Agent: Apidog/1.0.0 (https://apidog.com)' \
-#--header 'Content-Type: application/json' \
-#--header 'Accept: */*' \
-#--header 'Host: api.clore.ai' \
-#--header 'Connection: keep-alive' \
-#--data-raw '[
-#    {
-#        "order_id": '$order$',
-#        "price":'$price$'
-#    }
-#]'
 
 sleep 5s;
 #price=$(echo "$price + $step" | bc)
